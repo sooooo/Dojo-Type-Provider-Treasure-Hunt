43753876484cd9c8e9fbbc662a439817518fc6e5
Roni <roni.michaels@gmail.com>
2014-02-27 20:29:38 -0800

    End of dojo

--- Dojo/Script.fsx
+++ Dojo/Script.fsx
@@ -16,8 +16,42 @@ let wb = WorldBankData.GetDataContext()
 // Get specific indicator for a specific country at a given year
 wb.Countries.``Czech Republic``.Indicators.``Population (Total)``.[2000]
 // Get a list of countries in a specified region
-wb.Regions.``Euro area``.Countries
+// wb.Regions.``Euro area``.Countries
+let countries = wb.Regions.``North America``.Countries
+let smallest = 
+    countries 
+    |> Seq.map (fun c -> 
+        c.Name, 
+        [ for y in 2000 .. 2005 -> c.Indicators.``Life expectancy at birth, total (years)``.[y] ])
+    |> Seq.toArray
 
+let byYear = 
+    [ 2000 .. 2005 ]
+    |> List.map (fun y -> 
+        printfn "In %i " y
+        countries |> Seq.iter (fun c -> printfn "%s,%f" c.Name c.Indicators.``Life expectancy at birth, total (years)``.[y])
+        countries 
+        |> Seq.minBy (fun c -> c.Indicators.``Life expectancy at birth, total (years)``.[y]))
+//        |> fun c -> sprintf "%s" c.Name)
+
+let smallest2 = 
+    countries 
+    |> Seq.minBy (fun c -> c.Indicators.``Life expectancy at birth, total (years)``.[2000])
+    |> fun c -> c.Code.Substring(0,2)
+
+//    |> Seq.map (fun c -> (c.Code, c.Indicators.``Life expectancy at birth, total (years)``.[2000]))
+//    |> Seq.sortBy (fun (code,exp) -> exp)
+//    |> Seq.nth 0
+//    |> fun (code,exp) -> code.Substring(0,2)
+
+
+let f x = x + 5
+let data = [ 1 .. 10 ]
+data |> List.map f
+data |> List.map (fun x -> sprintf "Number %i" x)
+
+let g x y = printfn "%s,%i" x y
+42 |> g "Hello"
 
 // ------------------------------------------------------------------
 // WORD #2
@@ -28,9 +62,16 @@ wb.Regions.``Euro area``.Countries
 // ------------------------------------------------------------------
 
 // Create a type for working with XML documents based on a sample file
-type Sample = XmlProvider<"data/Writers.xml">
-// Load the sample document - explore properties using "doc."
+//type Sample = XmlProvider<"data/Writers.xml">
+type Sample = XmlProvider<"data/bbc.xml">
 let doc = Sample.GetSample()
+let x = doc.Channel.Items
+x |> Seq.filter (fun item -> (item.PubDate.Hour = 9) && (item.PubDate.Minute = 5))
+  |> Seq.map (fun item -> item.Title.Split(' '))
+  |> Seq.map (fun words -> 
+      let last = words.Length
+      words.[last-1])
+  |> Seq.toArray
 
 
 // ------------------------------------------------------------------
@@ -46,12 +87,20 @@ let doc = Sample.GetSample()
 // Using The MovieDB REST API
 // Make HTTP request to /3/search/person
 let key = "6ce0ef5b176501f8c07c634dfa933cff"
-let name = "craig"
+//let name = "craig"
+let name = "Haugerud"
+
+//let data = 
+//  Http.RequestString
+//    ( "http://api.themoviedb.org/3/search/person",
+//      query = [ ("query", "craig"); ("api_key", key) ],
+//      headers = ["accept", "application/json"] )
 let data = 
   Http.RequestString
     ( "http://api.themoviedb.org/3/search/person",
-      query = [ ("query", "craig"); ("api_key", key) ],
+      query = [ ("query", name); ("api_key", key) ],
       headers = ["accept", "application/json"] )
+
 
 // Parse result using JSON provider
 // (using sample result to generate types)
@@ -59,7 +108,7 @@ type PersonSearch = JsonProvider<"data/personsearch.json">
 let sample = PersonSearch.Parse(data)
 
 let first = sample.Results |> Seq.head
-first.Name
+first.Id
 
 // Request URL: "http://api.themoviedb.org/3/person/<id>/movie_credits
 // (You can remove the 'query' parameter because it is not needed here;
